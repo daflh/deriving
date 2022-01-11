75712f85931732bbf6134adf8935edf2ac6cda55
Dafiul Haq <dafiulhaq97@gmail.com>
2022-01-11 15:19:16 +0700

    add support to 12 new coins AVAX, BCH, BTG, BSV, DASH, DOGE, ETC, FTM, LTC, MATIC, BNB, VET

---
+++ src/js/bitcoinjs-networks.js
@@ -0,0 +1,56 @@
+import * as bitcoinLib from 'bitcoinjs-lib';
+
+export default {
+    bitcoin: bitcoinLib.networks.bitcoin,
+    bitcoinCash: bitcoinLib.networks.bitcoin,
+    bitcoinGold: {
+        messagePrefix: '\x1DBitcoin Gold Signed Message:\n',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x26,
+        scriptHash: 0x17,
+        wif: 0x80
+    },
+    bitcoinSv: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x00,
+        scriptHash: 0x05,
+        wif: 0x80
+    },
+    dash: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x4c,
+        scriptHash: 0x10,
+        wif: 0xcc
+    },
+    dogecoin: {
+        messagePrefix: '\x19Dogecoin Signed Message:\n',
+        bip32: {
+            public: 0x02facafd,
+            private: 0x02fac398
+        },
+        pubKeyHash: 0x1e,
+        scriptHash: 0x16,
+        wif: 0x9e
+    },
+    litecoin: {
+        messagePrefix: '\x19Litecoin Signed Message:\n',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x30,
+        scriptHash: 0x32,
+        wif: 0xb0
+    }
+};
--- src/js/keygen-util.js
+++ src/js/keygen-util.js
@@ -4,29 +4,38 @@ const keyGen = {};
 
 import * as bip32 from 'bip32';
 import * as bitcoinLib from 'bitcoinjs-lib';
+import bitcoinNetworks from './bitcoinjs-networks';
 
-keyGen.bitcoin = (seed, path) => {
-    const seedBuffer = Buffer.from(seed, 'hex');
-    const masterWallet = bip32.fromSeed(seedBuffer, bitcoinLib.networks.bitcoin);
-    const wallet = masterWallet.derivePath(path);
-    const publicKey = wallet.publicKey;
-    const { address } = bitcoinLib.payments.p2pkh({ pubkey: publicKey });
-
-    return { address, publicKey: publicKey.toString('hex'), privateKey: wallet.toWIF() };
-};
+for (const network in bitcoinNetworks) {
+    keyGen[network] = (seed, path) => {
+        const seedBuffer = Buffer.from(seed, 'hex');
+        const masterWallet = bip32.fromSeed(seedBuffer, bitcoinNetworks[network]);
+        const wallet = masterWallet.derivePath(path);
+        const publicKey = wallet.publicKey;
+        const { address } = bitcoinLib.payments.p2pkh({ pubkey: publicKey, network: bitcoinNetworks[network] });
+    
+        return { address, publicKey: publicKey.toString('hex'), privateKey: wallet.toWIF() };
+    };
+}
 
 import { hdkey } from 'ethereumjs-wallet';
 
-keyGen.ethereum = (seed, path) => {
-    const seedBuffer = Buffer.from(seed, 'hex');
-    const masterWallet = hdkey.fromMasterSeed(seedBuffer);
-    const wallet = masterWallet.derivePath(path).getWallet();
-    const address = wallet.getAddressString();
-    const publicKey = wallet.getPublicKeyString();
-    const privateKey = wallet.getPrivateKeyString();
+const ethereumBasedNetworks = [
+    'avalancheC', 'fantom', 'smartChain', 'ethereum', 'ethereumClassic', 'polygon', 'veChain'
+];
 
-    return { address, publicKey, privateKey };
-};
+ethereumBasedNetworks.forEach((network) => {
+    keyGen[network] = (seed, path) => {
+        const seedBuffer = Buffer.from(seed, 'hex');
+        const masterWallet = hdkey.fromMasterSeed(seedBuffer);
+        const wallet = masterWallet.derivePath(path).getWallet();
+        const address = wallet.getAddressString();
+        const publicKey = wallet.getPublicKeyString();
+        const privateKey = wallet.getPrivateKeyString();
+    
+        return { address, publicKey, privateKey };
+    };
+});
 
 import nacl from 'tweetnacl';
 import * as edHd from 'ed25519-hd-key';
